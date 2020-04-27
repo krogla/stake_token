c8decb23f1c2564d511c4b6f921e3003e350df70
KRogLA <i@krogla.me>
2020-04-27 08:00:44 +0200

    dependency update; project settings

--- networks.js
+++ networks.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -8,5 +11,13 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://rinkeby.infura.io/v3/${ process.env.INFURA_PROJECT_ID }`
+      ),
+      networkId: 4,
+      gasPrice: 10e9
+    }
   },
 };
